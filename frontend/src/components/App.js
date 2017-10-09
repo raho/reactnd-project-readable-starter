ef461572b23038ffff77519a7892674a38e186f2
Rafal Hotlos <rafal.hotlos@gmail.com>
2017-10-09 21:37:30 +0200

    fix: change category url to /:category

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import Navigation from './Navigation';
+import { connect } from 'react-redux';
+import { fetchCategories } from '../actions';
+import { withRouter } from 'react-router-dom';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.fetchCategories();
+  }
 
   render() {
     return (
       <div className="App">
         <Route exact path="/" component={Navigation}/>
-        <Route path="/cat/:cat" component={Navigation}/>
+        <Route path="/:category" component={Navigation}/>
       </div>
     );
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  fetchCategories: () => dispatch(fetchCategories())
+})
+
+export default withRouter(connect(null, mapDispatchToProps)(App));
--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchCategories } from '../actions'
 import { Link } from 'react-router-dom';
 
 class Navigation extends Component {
 
-  componentDidMount() {
-    this.props.fetchCategories();
-  }
-
   render() {
-    const currentCategoryPath = this.props.match.params.cat;
+    const currentCategoryPath = this.props.match.params.category;
     const { categories } = this.props;
     return (
       <nav className="navbar navbar-expand navbar-dark bg-black">
@@ -19,7 +14,7 @@ class Navigation extends Component {
           {categories.map(category => (
             <Link 
               key={category.path} className={'nav-item nav-link ' + (currentCategoryPath === category.path ? 'active' : '')} 
-              to={`/cat/${category.path}`}>{category.name}</Link>
+              to={`/${category.path}`}>{category.name}</Link>
           ))} 
         </div>
       </nav>
@@ -33,8 +28,4 @@ const mapStateToProps = ({categories}) => {
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  fetchCategories: () => dispatch(fetchCategories())
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(Navigation);
+export default connect(mapStateToProps)(Navigation);
